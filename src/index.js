8c424c5b57b7876a6e8450dc18bac82b0e341cb3
Angel <angel.m.young24@gmail.com>
2019-06-26 15:04:09 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function App() {
+    return ( <div >
+        <ToDoList / >
+        </div>
+        );
+}
+
+function ToDoList() {
+  const [text, setText] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+  const [finishedTodos, setFinishedTodos] = React.useState([]);
+
+  function addTodo() {
+    setTodos([...todos, text]);
+    setText("");
+  }
+
+  function finishTodo(todo) {
+    setFinishedTodos([...finishedTodos, todo]);
+  }
+
+  function startTodo(todo) {
+    setFinishedTodos(
+      finishedTodos.filter(finishedTodo => finishedTodo !== todo)
+    );
+  }
+
+  return (
+    <div>
+      <h1>Bucket List</h1>
+      {todos.map(todo => {
+        const isDone = finishedTodos.includes(todo);
+        return (
+          <h3 key={todo}>
+            <input
+              type="checkbox"
+              checked={isDone}
+              onChange={function(e) {
+                isDone ? startTodo(todo) : finishTodo(todo);
+              }}
+            />
+            {isDone ? <del>{todo}</del> : todo}  
+          </h3>
+        );
+      })}
+
+      <input
+        value={text}
+        onChange={function(e) {
+          setText(e.target.value);
+        }}
+      />
+
+      <button disabled={!text} onClick={addTodo}>
+        Add!
+      </button>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
