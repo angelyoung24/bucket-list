702d4f3124ed37f10b83f4ea1520f19e30bbd98d
Angel <angel.m.young24@gmail.com>
2019-06-26 15:43:19 -0400

    Updates

---
+++ precache-manifest.21cb64edf7d9e2bd088490bf2514b780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58ffef6f310e6940bf06cd003a4f089",
+    "url": "/bucket-list/index.html"
+  },
+  {
+    "revision": "924a820eca16cbed0f7f",
+    "url": "/bucket-list/static/css/main.38ebadf6.chunk.css"
+  },
+  {
+    "revision": "93c4319319f992928171",
+    "url": "/bucket-list/static/js/2.244b2b74.chunk.js"
+  },
+  {
+    "revision": "924a820eca16cbed0f7f",
+    "url": "/bucket-list/static/js/main.82948bea.chunk.js"
+  },
+  {
+    "revision": "22a2fef89e016e59df17",
+    "url": "/bucket-list/static/js/runtime~main.934c4c2b.js"
+  }
+]);
